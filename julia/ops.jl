b58a02f3c01cdde76e946dcabf468fbeea8cbe32
bornarravlicb <240896699+bornarravlicb@users.noreply.github.com>
2026-02-10 16:05:26 +0100

    Added basic functionality to ops.jl

---
+++ julia/ops.jl
@@ -0,0 +1,55 @@
+
+module Activations
+export linear, relu, sigmoid, tanh
+
+linear(x) = x
+
+relu(x) = max.(0, x)
+
+sigmoid(x) = 1 ./ (1 .+ exp.(-x))
+
+tanh(x) = (exp.(x) .- exp.(-x)) ./ (exp.(x) .+ exp.(-x))
+
+end
+
+
+module Layers
+export forward, backward
+
+function forward(x, w, b)
+    return w * x .+ b
+end
+
+function backward(dz, x, w)
+    dW = dz * x'
+    db = sum(dz, dims=2)
+    dx = w' * dz
+    return dx, dW, db
+end
+
+end
+
+
+module Losses
+export mse, dmse
+using Statistics
+
+function mse(y_pred, y_true)
+    return mean((y_pred .- y_true).^2)
+end
+
+function dmse(y_pred, y_true)
+    return 2 .* (y_pred .- y_true) ./ length(y_true)
+end
+
+end
+
+module Optimizers
+export update_params!
+
+function update_params!(w, b, dw, db, lr)
+    w .-= lr .* dw
+    b .-= lr .* db
+end
+
+end
